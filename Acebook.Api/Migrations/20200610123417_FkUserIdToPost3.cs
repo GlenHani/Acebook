fa7c7bc46a45074b8072348dec0001ea8b12fa9b
GlenHani <glen.hani@beis.gov.uk>
2020-06-10 13:35:03 +0100

    Done

---
+++ Acebook.Api/Migrations/20200610123417_FkUserIdToPost3.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcebookApi.Migrations
+{
+    public partial class FkUserIdToPost3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UsersId",
+                table: "Posts",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_UsersId",
+                table: "Posts",
+                column: "UsersId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_UsersId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_UsersId",
+                table: "Posts");
+        }
+    }
+}
