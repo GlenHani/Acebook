e6a663928ebb6ded02dea7dffe1604cdd74cb976
GlenHani <glen.hani@beis.gov.uk>
2020-06-12 11:35:37 +0100

    Commit

---
+++ Acebook.Api/Migrations/20200612103156_UserNameAndPasswordMand.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcebookApi.Migrations
+{
+    public partial class UserNameAndPasswordMand : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
