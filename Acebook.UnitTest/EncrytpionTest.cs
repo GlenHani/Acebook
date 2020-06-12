66073671180f23c2cb22cff2dfeef1f0911f9db7
GlenHani <glen.hani@beis.gov.uk>
2020-06-12 11:06:37 +0100

    * Acebook.Api.sln: * EncrytpionTest.cs: * Acebook.UnitTest.csproj: * EncrytpionRepository.cs:
    * Post.cs: Encyrption done with unit test

---
+++ Acebook.UnitTest/EncrytpionTest.cs
@@ -0,0 +1,44 @@
+using Acebook.Api.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acebook.UnitTest
+{
+    [TestClass]
+    public class EncrytpionTest
+    {
+        [TestMethod]
+        public void Encyrption()
+        {
+
+            // Arrange
+
+            string password = "Test";
+            var encyrption = new EncrytpionRepository(password);
+
+            // Act
+            var result = encyrption.ReturnEncrpyt();
+
+            // Assert
+         
+            Assert.AreEqual(result, "MGzYMsUyPHfnIfSDNsdRrQ==");
+        }
+
+        [TestMethod]
+        public void Dencrpyt()
+        {
+
+            // Arrange
+
+            string dytePassword = "MGzYMsUyPHfnIfSDNsdRrQ==";
+            var encyrption = new EncrytpionRepository(dytePassword);
+
+            // Act
+            var result = encyrption.ReturnDencrpyt();
+
+            // Assert
+
+            Assert.AreEqual(result, "Test");
+        }
+
+    }
+}
